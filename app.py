a507b6a65c9a33baf9cf507e74272baa254d8048
Alwin George <alwingeorge11@gmail.com>
2021-09-09 20:53:37 +0930

    adding a secret key

--- app.py
+++ app.py
@@ -3,8 +3,10 @@ import os
 import psycopg2
 
 DB_URL = os.environ.get("DATABASE_URL", "dbname=bugger")
+SECRET_KEY = os.environ.get("SECRET_KEY", "pretend key for testing only")
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = SECRET_KEY
 
 @app.route('/')
 def index():
